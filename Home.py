b6005873c3986f4fdfad9a804a42a0d18cc0ec9e
Adit Bhosle <aditbhosle81@gmail.com>
2024-01-17 16:34:18 -0500

    Updated Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import subprocess
+
+subprocess.Popen(["uvicorn", "app:app", "--reload"])
 
 # page headings
 st.set_page_config(layout="wide", page_title="INFO7374: Algorithmic Marketing")
